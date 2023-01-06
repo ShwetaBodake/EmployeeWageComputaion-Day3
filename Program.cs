0251cb0e39076b2677ab2cce9f9a50f7e267ce7c
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-06 18:56:16 +0530

    Welcome Message is Displayed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+namespace EmployeeWageComputation
+{
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To Employee Wage Computaion Program");
+        }
+    }
+}
