6d1c84b847d78a5d5396305939fc8b75769c9ac3
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-06 19:00:01 +0530

    Employee is present or not is checked

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ public class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Employee Wage Computaion Program");
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int EMP_Check = random.Next(0, 2);
+
+            if (EMP_Check == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+                Console.WriteLine("Employee is Absent.");
         }
     }
 }
