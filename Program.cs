83521f0210c7486cbeeaee93610426f008510226
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-06 19:32:25 +0530

    Part time employee Wage is calculated

--- Program.cs
+++ Program.cs
@@ -8,43 +8,31 @@ public class Program
         {
             Console.WriteLine("WelCome To Employee Wage Computaion Program");
 
-
-            int Is_Part_Time = 2;
-            int Is_Full_Time = 1;
-            int Emp_Wage_Per_Hour = 20;
-            int Max_Hours_In_Month = 100;
-            int Working_Days_In_Month = 20;
+            int Wage_Per_Hour = 20;
+            int Part_Time_Hour = 8;
+            int Full_Time_hour = 2 * Part_Time_Hour;
 
 
-
-            int empHours = 0;
-            int totalEmpHours = 0;
-            int totalWorkingDays = 0;
-
-            while (totalEmpHours <= Max_Hours_In_Month && totalWorkingDays < Working_Days_In_Month)
+            Random random = new Random();
+            int CheckEmp = random.Next(0, 3);
+            if (CheckEmp == 1)
             {
-                totalWorkingDays++;
+                Console.WriteLine("Employee is Fulltime Present");
+                int Daily_Wage_Of_Fulltime = Wage_Per_Hour * Full_Time_hour;
+                Console.WriteLine("Daily wage of full time Employee is " + Daily_Wage_Of_Fulltime);
 
-                Random random = new Random();
-                int CheckEmp = random.Next(0, 3);
-
-                switch (CheckEmp)
-                {
-                    case 1:
-                        empHours = 8;
-                        break;
-                    case 2:
-                        empHours = 4;
-                        break;
-                    default:
-                        empHours = 0;
-                        break;
-                }
-                totalEmpHours += empHours;
-                Console.WriteLine("Days:- " + totalWorkingDays + "EmpHours:- " + empHours);
             }
-            int totalEmpWage = totalEmpHours * Emp_Wage_Per_Hour;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            else if (CheckEmp == 2)
+            {
+                Console.WriteLine("Employee is part time present");
+                int Daily_Wage_Of_Parttime = Wage_Per_Hour * Part_Time_Hour;
+                Console.WriteLine("Daily wage of part time Employee is " + Daily_Wage_Of_Parttime);
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
