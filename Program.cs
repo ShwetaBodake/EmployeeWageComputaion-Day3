0c6b21d800003429245b293f984a046fa2e511d1
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-06 19:07:52 +0530

    Part Employee Wage Is Calculated

--- Program.cs
+++ Program.cs
@@ -7,17 +7,26 @@ public class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Employee Wage Computaion Program");
-            int Wage_per_Hour = 20;
-            int Full_Day_Hour = 8;
-            Random random = new Random();
-            int isPresent = random.Next(0, 2);
 
-            //Console.WriteLine(isPresent);
-            if (isPresent == 1)
+            int Wage_Per_Hour = 20;
+            int Part_Time_Hour = 8;
+            int Full_Time_hour = 2 * Part_Time_Hour;
+
+
+            Random random = new Random();
+            int CheckEmp = random.Next(0, 3);
+            if (CheckEmp == 1)
             {
-                Console.WriteLine("Employee is PRESENT");
-                int DailyWge = Wage_per_Hour * Full_Day_Hour;
-                Console.WriteLine("Daily wage of employee is " + DailyWge);
+                Console.WriteLine("Employee is Fulltime Present");
+                int Daily_Wage_Of_Fulltime = Wage_Per_Hour * Full_Time_hour;
+                Console.WriteLine("Daily wage of full time Employee is " + Daily_Wage_Of_Fulltime);
+
+            }
+            else if (CheckEmp == 2)
+            {
+                Console.WriteLine("Employee is part time present");
+                int Daily_Wage_Of_Parttime = Wage_Per_Hour * Part_Time_Hour;
+                Console.WriteLine("Daily wage of part time Employee is " + Daily_Wage_Of_Parttime);
 
             }
             else
